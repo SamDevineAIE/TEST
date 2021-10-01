b8dde2b9e6912ec5d37a5436881f1e721db11a42
DESKTOP-U1R54I9\User <s200027@students.aie.edu.au>
2021-10-01 14:54:33 +0930

    01/10/2021 update

---
+++ TEST project/Assets/Scripts/QuitButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+
+    public void hasquit()
+    {
+        Debug.Log("has quit");
+        Application.Quit();
+
+    }
+}
